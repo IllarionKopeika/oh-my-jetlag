29b192183353abff8eacd63dcc898c2b6fe39e09
Illarion Kopeika <larrypearson@yandex.ru>
2025-10-20 15:13:54 +0800

    password visibility and datepicker

---
+++ app/javascript/controllers/datepicker_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="datepicker"
+export default class extends Controller {
+  connect() {
+    flatpickr.localize(flatpickr.l10ns.ru)
+    flatpickr(this.element, {
+      position: 'auto center',
+      minDate: '2021-09-23',
+      maxDate: new Date().fp_incr(365),
+      dateFormat: 'd-M-Y'
+    })
+  }
+}
