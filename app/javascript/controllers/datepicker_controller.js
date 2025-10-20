2c7275ffad256b8da73c8e60707a5c8021d05e9a
Illarion Kopeika <larrypearson@yandex.ru>
2025-10-20 16:18:50 +0800

    datepicker human-friendly date

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -4,10 +4,15 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
     flatpickr.localize(flatpickr.l10ns.ru)
+
+    const locale = this.element.dataset.locale
+
     flatpickr(this.element, {
       position: "auto center",
       minDate: "2021-09-23",
       maxDate: new Date().fp_incr(365),
+      altInput: true,
+      altFormat: locale === "en" ? "M j, Y" : "j M Y",
       dateFormat: "d-M-Y",
       disableMobile: "true"
     })
