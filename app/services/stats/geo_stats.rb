e7760af95c22b130c1626faa0e29cf3bbd8af4aa
Illarion Kopeika <larrypearson@yandex.ru>
2026-04-01 13:28:31 +0800

    sorting rules for visited countries in geo stats

--- app/services/stats/geo_stats.rb
+++ app/services/stats/geo_stats.rb
@@ -26,26 +26,53 @@ class Stats::GeoStats
 
   # countries
   def geo_tree
-    Continent.includes(regions: { countries: :airports }).map do |continent|
+    locale = I18n.locale.to_s
+    visited_countries = Airport.where(id: visited_airports).distinct.pluck(:country_id)
+
+    continents = Continent.includes(regions: { countries: :airports }).map do |continent|
+      regions = continent.regions.map do |region|
+        countries = region.countries.map do |country|
+          visited = visited_countries.include?(country.id)
+          {
+            id: country.id,
+            name: country.name,
+            code: country.code,
+            flag_url: country.flag_url,
+            visited: visited
+          }
+        end
+        visited_count = countries.count { |country| country[:visited] }
+        {
+          id: region.id,
+          name: region.name,
+          countries: countries.sort_by do |country|
+            [
+              country[:visited] ? 0 : 1,
+              country[:name][locale]
+            ]
+          end,
+          visited_count: visited_count
+        }
+      end
+      visited_count = regions.sum { |region| region[:visited_count] }
       {
         id: continent.id,
         name: continent.name,
-        regions: continent.regions.map do |region|
-          {
-            id: region.id,
-            name: region.name,
-            countries: region.countries.map do |country|
-              {
-                id: country.id,
-                name: country.name,
-                code: country.code,
-                flag_url: country.flag_url,
-                visited: country.airports.where(id: visited_airports).exists?
-              }
-            end
-          }
-        end
+        regions: regions.sort_by do |region|
+          [
+            -region[:visited_count],
+            region[:name][locale]
+          ]
+        end,
+        visited_count: visited_count
       }
+    end
+
+    continents.sort_by do |continent|
+      [
+        -continent[:visited_count],
+        continent[:name][locale]
+      ]
     end
   end
 end
